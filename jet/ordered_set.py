77846a9bb90f0b7c536f6850c242714ffa9f2b4f
Sushil Khanchi <sushilkhanchi97@gmail.com>
2023-02-20 12:07:03 +0530

    FIX :: MutableSet AttributeError

--- jet/ordered_set.py
+++ jet/ordered_set.py
@@ -1,11 +1,11 @@
 import collections
 
 
-class OrderedSet(collections.MutableSet):
+class OrderedSet(collections.abc.MutableSet):
     def __init__(self, iterable=None):
         self.end = end = []
-        end += [None, end, end]         # sentinel node for doubly linked list
-        self.map = {}                   # key --> [key, prev, next]
+        end += [None, end, end]  # sentinel node for doubly linked list
+        self.map = {}  # key --> [key, prev, next]
         if iterable is not None:
             self |= iterable
 
